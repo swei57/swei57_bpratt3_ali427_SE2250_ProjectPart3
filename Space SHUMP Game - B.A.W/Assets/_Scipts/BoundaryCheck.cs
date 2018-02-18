95af943e30d412239486b5f20da7362449ae0b17
swei57 <swei57@users.noreply.github.com>
2018-02-18 17:32:37 -0500

    Space ship moves, added boundaries to main camera.

---
+++ Space SHUMP Game - B.A.W/Assets/_Scipts/BoundaryCheck.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Keeps player on the screen
+public class BoundaryCheck : MonoBehaviour {
+    public float radius = 1f;
+    public float cameraWidth;
+    public float cameraHeight;
+	// Use this for initialization
+	void Awake () {
+        cameraHeight = Camera.main.orthographicSize;
+        cameraWidth = cameraHeight * Camera.main.aspect;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 position = transform.position;
+        if(position.x > cameraWidth - radius){
+            position.x = cameraWidth - radius;
+        }
+        if (position.x < -cameraWidth + radius){
+            position.x = -cameraWidth + radius;
+        }
+        if (position.y > cameraHeight - radius){
+            position.y = cameraHeight - radius;
+        }
+        if (position.y > -cameraHeight + radius){
+            position.y = -cameraHeight + radius;
+        }
+        transform.position = position;
+    }
+
+    void OnDrawGizmos()
+    {
+        if (!Application.isPlaying) return;
+        Vector3 boundSize = new Vector3(cameraWidth * 2, cameraHeight * 2, 0.1f);
+        Gizmos.DrawWireCube(Vector3.zero, boundSize);
+    }
+}
---
+++ Space SHUMP Game - B.A.W/Assets/_Scipts/playerShip.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerShip : MonoBehaviour {
+
+    static public playerShip ship;   //setting ship default vals
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+    public float shieldLevel = 1;
+    // Use this for initialization
+    void Awake() {
+        if(ship == null){
+            ship = this;   //set ship singleton
+        }
+        else{
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.Ship!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 position = transform.position;      //change player position based on axis
+        position.x += xAxis * speed * Time.deltaTime;
+        position.y += yAxis * speed * Time.deltaTime;
+        transform.position = position;
+
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult,0); //ship rotation
+    }
+}

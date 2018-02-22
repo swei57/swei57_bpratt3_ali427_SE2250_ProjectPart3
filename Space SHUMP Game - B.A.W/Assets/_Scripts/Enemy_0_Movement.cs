84163621ab845985cd3e0d147e4c5f8c243e218c
amyli071 <amy.lyq168@gmail.com>
2018-02-21 23:50:26 -0500

    Enemy_1 Updates
    Enemy_1 now moves in 45 degree angle downwards

--- Space SHUMP Game - B.A.W/Assets/_Scripts/Enemy_0_Movement.cs
+++ Space SHUMP Game - B.A.W/Assets/_Scripts/Enemy_0_Movement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy_0_Movement : MonoBehaviour {
 
-	public float speed = .1f;
+	private float speed = .1f;
 	private Vector3 direction = new Vector3(0f,-1f,0f);
 
 	// Use this for initialization
---
+++ Space SHUMP Game - B.A.W/Assets/_Scripts/Enemy_1_Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_1_Movement : MonoBehaviour {
+
+	private float speed = .1f;
+
+	public Vector3 ChooseDirection;
+	
+	// Update is called once per frame
+	void Update () {
+
+		System.Random random = new System.Random();
+
+		float i = Random.value;
+		if(i==0){
+			ChooseDirection.Set (-1, -1, 0);
+		}
+		else {
+			ChooseDirection.Set (1f, -1f, 0f);
+		}
+
+		transform.Translate (ChooseDirection * speed);
+
+	}
+
+
+			
+}

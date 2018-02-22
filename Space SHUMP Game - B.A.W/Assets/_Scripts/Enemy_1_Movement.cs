7acdc19dd718a012de9deb9914ee06cdb0a4003d
amyli071 <amy.lyq168@gmail.com>
2018-02-22 09:54:24 -0500

    Updated Enemy_1 Movement
    Enemy_1 now moves randomly at 45 degrees downwards

--- Space SHUMP Game - B.A.W/Assets/_Scripts/Enemy_1_Movement.cs
+++ Space SHUMP Game - B.A.W/Assets/_Scripts/Enemy_1_Movement.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 public class Enemy_1_Movement : MonoBehaviour {
 
 	private float speed = .1f;
+	private int x, y, z;
 
 	public Vector3 ChooseDirection;
-	
+
+	void Start(){
+		float i = Random.value;
+
+		if(i<=0.5){
+			x = -1;
+			y = -1;
+			z = 0;
+		}
+		else {
+			x = 1;
+			y = -1;
+			z = 0;
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 
-		System.Random random = new System.Random();
+		//System.Random random = new System.Random();
 
-		float i = Random.value;
-		if(i==0){
-			ChooseDirection.Set (-1, -1, 0);
-		}
-		else {
-			ChooseDirection.Set (1f, -1f, 0f);
-		}
-
+		ChooseDirection.Set (x, y, z);
 		transform.Translate (ChooseDirection * speed);
 
 	}

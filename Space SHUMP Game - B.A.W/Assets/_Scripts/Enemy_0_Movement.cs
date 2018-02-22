d478f4f3f180eb4e278aeed130f72b3b72c1514d
amyli071 <amy.lyq168@gmail.com>
2018-02-21 23:20:34 -0500

    Enemy0 Updates
    Enemy 0 movement added

---
+++ Space SHUMP Game - B.A.W/Assets/_Scripts/Enemy_0_Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_0_Movement : MonoBehaviour {
+
+	public float speed = .1f;
+	private Vector3 direction = new Vector3(0f,-1f,0f);
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (direction * speed);
+}
+}
